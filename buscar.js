57451f71c05faa3eead62c4f710fe85b5c49b8ab
loquendero <93691574+TIORADIO@users.noreply.github.com>
2023-04-09 15:34:08 -0400

    Update buscar.js

--- buscar.js
+++ buscar.js
@@ -1,76 +1,67 @@
-// obtener los datos del formulario
-var titulo = document.getElementById('titulo').value;
-var autor = document.getElementById('autor').value;
-var dia = document.getElementById('dia').value;
-var mes = document.getElementById('mes').value;
-var anio = document.getElementById('anio').value;
-var genero = document.getElementById('genero').value;
-
-// definir la ruta de la carpeta con las noticias
-var carpeta = 'noticias/';
-
-// obtener una lista de los archivos HTML en la carpeta
-var archivos = [  'noticias/noticia1.html',  'noticias/noticia2.html',  'noticias/noticia3.html'];
-
-// crear una tabla para mostrar los resultados
-var resultados_encontrados = false;
-var tabla_resultados = document.createElement('table');
-var fila_encabezado = document.createElement('tr');
-var encabezados = ['Título', 'Autor', 'Fecha', 'Género'];
-for (var i = 0; i < encabezados.length; i++) {
-  var celda_encabezado = document.createElement('th');
-  celda_encabezado.textContent = encabezados[i];
-  fila_encabezado.appendChild(celda_encabezado);
-}
-tabla_resultados.appendChild(fila_encabezado);
-
-for (var i = 0; i < archivos.length; i++) {
-  // leer los metatags del archivo HTML
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', archivos[i], false);
-  xhr.send();
-  var metatags = parsear_meta_tags(xhr.responseText);
-
-  // obtener el día, mes y año del metatag
-  var fecha_metatag = new Date(metatags['date']);
-  var dia_metatag = fecha_metatag.getDate();
-  var mes_metatag = fecha_metatag.getMonth() + 1;
-  var anio_metatag = fecha_metatag.getFullYear();
-
-  // verificar si el archivo coincide con los criterios de búsqueda
-  if (
-    (titulo == '' || metatags['title'].toLowerCase().indexOf(titulo.toLowerCase()) != -1) &&
-    (autor == '' || metatags['author'].toLowerCase().indexOf(autor.toLowerCase()) != -1) &&
-    (dia == '' || dia_metatag == dia) &&
-    (mes == '' || mes_metatag == mes) &&
-    (anio == '' || anio_metatag == anio) &&
-    (genero == '' || metatags['genre'].toLowerCase() == genero.toLowerCase())
-  ) {
-    // mostrar los datos en la tabla
-    var fila = document.createElement('tr');
-    var celda_titulo = document.createElement('td');
-    var enlace = document.createElement('a');
-    enlace.href = archivos[i];
-    enlace.textContent = metatags['title'];
-    celda_titulo.appendChild(enlace);
-    fila.appendChild(celda_titulo);
-    var celda_autor = document.createElement('td');
-    celda_autor.textContent = metatags['author'];
-    fila.appendChild(celda_autor);
-    var celda_fecha = document.createElement('td');
-    celda_fecha.textContent = metatags['date'];
-    fila.appendChild(celda_fecha);
-    var celda_genero = document.createElement('td');
-    celda_genero.textContent = metatags['genre'];
-    fila.appendChild(celda_genero);
-    tabla_resultados.appendChild(fila);
-    resultados_encontrados = true;
-  }
-}
-
-if (!resultados_encontrados) {
-  window.location.href = 'https://www.volveralahemeroteca.com';
-} else {
-  document.body.appendChild(tabla_resultados);
-}
-
+$(document).ready(function() {
+  $('#form-busqueda').submit(function(e) {
+    e.preventDefault(); // Evita que se recargue la página al enviar el formulario
+    var resultados = [];
+    var titulo = $('#titulo').val();
+    var genero = $('#genero').val();
+    var autor = $('#autor').val();
+    var dui = $('#dui').val();
+    var mos = $('#mos').val();
+    var chovo = $('#chovo').val();
+    $.ajax({
+      url: 'noticias/',
+      success: function(data) {
+        $(data).find('a').attr('href', function(i, val) {
+          if (val.match(/\.html$/)) { // Solo buscamos archivos HTML
+            $.ajax({
+              url: 'noticias/' + val,
+              success: function(htmlData) {
+                var encontrado = true;
+                $('meta[name="title"]', htmlData).each(function() {
+                  if (titulo && $(this).attr('content').toLowerCase().indexOf(titulo.toLowerCase()) === -1) {
+                    encontrado = false;
+                    return false;
+                  }
+                });
+                $('meta[name="genre"]', htmlData).each(function() {
+                  if (genero && $(this).attr('content').toLowerCase() !== genero.toLowerCase()) {
+                    encontrado = false;
+                    return false;
+                  }
+                });
+                $('meta[name="author"]', htmlData).each(function() {
+                  if (autor && $(this).attr('content').toLowerCase().indexOf(autor.toLowerCase()) === -1) {
+                    encontrado = false;
+                    return false;
+                  }
+                });
+                $('meta[name="lola"]', htmlData).each(function() {
+                  if ((dui && $(this).attr('content') !== dui) || (mos && $(this).attr('content') !== mos) || (chovo && $(this).attr('content') !== chovo)) {
+                    encontrado = false;
+                    return false;
+                  }
+                });
+                if (encontrado) {
+                  resultados.push('<a href="' + val + '">' + val + '</a>');
+                }
+              },
+              error: function() {
+                $('#resultados').html('Error al cargar el archivo ' + val);
+              }
+            });
+          }
+        });
+        setTimeout(function() {
+          if (resultados.length > 0) {
+            $('#resultados').html(resultados.join('<br>'));
+          } else {
+            $('#resultados').html('No se encontraron archivos');
+          }
+        }, 1000); // Espera 1 segundo para que se completen todas las búsquedas antes de mostrar los resultados
+      },
+      error: function() {
+        $('#resultados').html('Error al cargar los archivos');
+      }
+    });
+  });
+});
